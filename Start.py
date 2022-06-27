608636ab475692beb21cabac5f62bfcbc4985e3c
Andrey <S2rancia13Git>
2022-06-27 22:30:17 +0300

    Init new Start.py

---
+++ Start.py
@@ -0,0 +1,10 @@
+end = False
+
+
+def main_loop(name, n_players):
+    while not end:  # not 8 buildings in the game for one player
+        print(1)
+
+
+if __name__ == "__main__":
+    main_loop("AZ", 6)
