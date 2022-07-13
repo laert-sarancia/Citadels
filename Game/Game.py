c3bd32c9063439568f80641083b2991a7847af97
Andrey <S2rancia13Git>
2022-07-13 17:02:48 +0300

    Init new Start.py

---
+++ Game/Game.py
@@ -0,0 +1,9 @@
+class Game:
+    max_building_amount = 8
+
+    def __int__(self):
+        self.max_players = 9
+
+    def set_max_players(self, max_players):
+        self.max_players = max_players
+
--- Start.py
+++ Start.py
@@ -1,10 +1,27 @@
-end = False
+from Game import Game
+
+exit = False
+
+def start() -> str:
+    return input(  # ToDo extend description
+        '''Welcome to The Citadels
+        Enter your name:''')
 
 
-def main_loop(name, n_players):
-    while not end:  # not 8 buildings in the game for one player
-        print(1)
+def settings() -> int:
+    n_pl = int(input("Set number of players"))
+    return n_pl
+
+
+def main_loop(name):
+    game = 0
+    while not exit:
+        game += 1
+        print(f"Game #{game} started.")
+        answer = int(input("Do you want to start again? Y\N:"))
+
 
 
 if __name__ == "__main__":
-    main_loop("AZ", 6)
+    name = start()
+    main_loop(name)
