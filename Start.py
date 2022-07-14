da1ee1cc085780963ecf5adbcae192c398646b77
Andrey <S2rancia13Git>
2022-07-14 15:59:08 +0300

    Structure of Start.py has been completed.

--- Start.py
+++ Start.py
@@ -4,8 +4,12 @@ exit = False
 
 def start() -> str:
     return input(  # ToDo extend description
-        '''Welcome to The Citadels
-        Enter your name:''')
+        '''
+        Welcome to The Citadels
+        It's a German-style card game, designed by Bruno Faidutti
+        The game was ported on PC by Andrey Zolotin
+         
+Enter your name:''')
 
 
 def settings() -> int:
@@ -15,11 +19,14 @@ def settings() -> int:
 
 def main_loop(name):
     game = 0
+    global exit
     while not exit:
         game += 1
         print(f"Game #{game} started.")
-        answer = int(input("Do you want to start again? Y\N:"))
-
+        answer = input("Do you want to start again? Y/N:")
+        if answer.lower() == "n":
+            exit = True
+    print(f"Goodbye, {name}!")
 
 
 if __name__ == "__main__":
