c658ae26456ed04e6db49c836af89402de737061
Andrey <S2rancia13Git>
2022-07-15 13:51:28 +0300

    Added building.py

---
+++ Game/building.py
@@ -0,0 +1,9 @@
+class Building:
+
+    def __init__(self, name: str, price: int, color: str, property: str):
+        self.name = name
+        self.price = price
+        self.color = color
+        self.property = property
+
+    #def building_name():
